d9dcd153adaebe9dec1e284893ab664ab16698e5
Marko Krizmancic <marko.krizmancic@fer.hr>
2023-11-07 09:58:35 +0100

    Update HTTP API client

--- mu_interface/Utilities/HTTP_client.py
+++ mu_interface/Utilities/HTTP_client.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 #from datetime import datetime
+import datetime
+import logging
 import json
-from pathlib import Path
 import time
+from pathlib import Path
+
 import requests
 import yaml
-import datetime
+from requests.adapters import HTTPAdapter, Retry
 
+# TODO: remove
+from mu_interface.Utilities.log_formatter import setup_logger
 
-#http://161.53.68.176:8000/"
+# TODO: error handling
+
 url = "https://stupefied-poitras.185-23-116-208.plesk.page/api/"
 headers = {"Content-Type": "application/json", "Authorization": "Basic dGhpc19pc190aGVfcGFzc3dvcmQ="}
 
@@ -19,116 +25,246 @@ class HTTPClient(object):
         # Get the list of nodes currently on the website.
         self.node_handle = node_handle
         self.display_name = display_name
-                
-        if not self.node_exists(node_handle):
-            self.add_node(node_handle, display_name)
         
+        self.session = requests.Session()
+        retries = Retry(total=3, backoff_factor=0.5)
+        self.session.mount('http://', HTTPAdapter(max_retries=retries))
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
+        
+        self.known_data_fields = None
+        self.known_nodes = None
+                
+        if not self.node_exists():
+            self.register_node()
+        
+    # DONE
+    def get_nodes(self):
+        """Get the list of nodes currently on the website."""
+        query = 'nodes'
 
+        try:
+            response = self.session.get(url + query, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while getting the list of nodes.")
+            return None
+        
+        parsed = json.loads(response.text)
+        self.known_nodes = [x['handle'] for x in parsed['data']]
+        return parsed['data']
+
+    # DONE
     def node_exists(self, node_handle=None):
         """
-        Checks if a given node already exists in the website/database
+        Check if a given node already exists on the website.
 
         Args:
-            node_handle (str): handle of node to check, if None checks self
+            node_handle (str): Handle of the node to check. If None checks self.
 
         Returns:
-            True if node exists, False otherwise
+            True if node exists, False otherwise.
         """
         if node_handle is None:
             node_handle = self.node_handle
 
-        query = 'nodes'
-        response = None
-        while response is None:
-            try:
-                response = requests.request("GET", url + query, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to GET current list of nodes. Trying again.")
-                time.sleep(0.5)
-        parsed = json.loads(response.text)
-        # print(parsed['data'])
-        return any(node['handle'] == node_handle for node in parsed['data'])
-        
+        if self.known_nodes is None:
+            self.get_nodes()
+        return node_handle in self.known_nodes
 
-    def add_node(self, node_handle, display_name): #node_handle has to be a string with letters!!!
+    # DONE
+    def add_node(self, node_handle, display_name):
         """
-        Adds a new node to the website/database
+        Add a new node to the website.
 
         Args:
             node_handle (str): Internal identifier of the node. Important: The string has to contain letters
                                 to avoid an error on the website. Don't only use numbers, even if they are 
-                                formatted as string it will still lead to problems
-            display_name (str): Name of the node that is shown on the website
+                                formatted as string it will still lead to problems.
+            display_name (str): Name of the node that is shown on the website.
 
         Returns:
-            True if a successful response is received, False otherwise
+            True if a successful response is received, False otherwise.
         """
-        if node_handle is None:
-            node_handle = self.node_handle
-        if display_name is None:
-            display_name = self.display_name
-        
         query = 'nodes'
         payload = {
             'handle': node_handle,
             'name': display_name
         }
         response = None
-        while response is None:
-            try:
-                response = requests.request("POST", url + query, json=payload, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to POST the new node. Trying again.")
+        try:
+            response = requests.request("POST", url + query, json=payload, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting to add the new node.")
+            return False
         
         if not response.ok:
-            print(f"ERROR: Adding node. Status code {response.status_code}")
+            logging.error(f"Failed to add node {node_handle}. Status code {response.status_code}")
             return False
             
         return True
 
-    
-    def add_data_field(self, field_name, field_handle, unit, node_handle=None):
+    # DONE
+    def register_node(self):
+        """Add a node with parameters specified in the constructor."""
+        self.add_node(self.node_handle, self.display_name)
+
+    # DONE
+    def delete_node(self, node_handle):
         """
-        Does nothing? TODO
+        Delete the node with the matching handle.
 
         Args:
-            field_name (str): Name of the data field that is shown on the website
-            field_handle (str): Internal identifier of the data field
-            unit (str): Unit of the datatype measured. To display on the website
-            node_handle (str): Is this really neaded? TODO
+            node_handle (str): Handle of node to delete.
 
         Returns:
-            True if a successful response is received, False otherwise
+            True if a successful response is received, False otherwise.
         """
-        if node_handle is None:
-            node_handle = self.node_handle
-            
+        query = 'nodes/delete'
+        payload = {
+            "handle": node_handle
+        }
+
+        try:
+            response = self.session.post(url + query, json=payload, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting to delete node.")
+            return False
+        
+        return True
+
+    # DONE
+    def get_data_fields(self):
+        """Return all available data fields on the website."""
+        query = 'data-field'
+        response = None
+        try:
+            response = self.session.get(url + query, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting for the current list of data fields.")
+            return None
+
+        parsed = json.loads(response.text)
+        self.known_data_fields = [x['handle'] for x in parsed['data']]
+        return parsed['data']
+
+    # DONE
+    def add_data_field(self, field_handle, field_name, unit):
+        """
+        Specify a new data field to be displayed on the website.
+
+        Args:
+            field_handle (str): Internal identifier of the data field.
+            field_name (str): Name of the data field that is shown on the website.
+            unit (str): Unit of the data field that is shown on the website.
+
+        Returns:
+            True if a successful response is received, False otherwise.
+        """
         query = 'data-field'
         payload = {
             "name": field_name,
             "handle": field_handle,
             "unit": unit
         }
-        response = None
-        while response is None:
-            try:
-                response = requests.request("POST", url + query, json=payload, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to POST the new data field. Trying again.")
-        
+
+        try:
+            response = self.session.post(url + query, json=payload, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting to add the new data field.")
+            return False
+                
         if not response.ok:
-            print(f"ERROR: Adding data field. Status code {response.status_code}")
+            logging.error(f"Failed to add data field {field_handle}. Status code {response.status_code}")
             return False
         
         return True
     
-
-    def add_data(self, data, additional_sensors, node_handle=None):
-
-        ## TODO : wait for Grants final API version, then adapt name of fields/additional sensors
-
+    # DONE
+    def delete_data_field(self, data_handle):
         """
-        Adds a single measurement set to the database
+        Delete data field with matching handle.
+
+        Args:
+            data_handle (str): Handle of data field to delete.
+
+        Returns:
+            True if a successful response is received, False otherwise.
+        """
+        query = 'data-field/delete'
+        payload = {
+            "handle": data_handle
+        }
+
+        try:
+            response = self.session.post(url + query, json=payload, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting to delete data field.")
+            return False
+        
+        return True
+    
+    # FIXME: why is data type needed
+    def get_data(self, data_type, date_range, node_handles=None,):
+        """
+        Return all data entries from specified nodes.
+
+        Args:
+            data_type (str): Type of data to retrieve.
+            date_range (str): Time range to retrieve data from.
+                              Possible values: 'last_hour', 'last_day', 'month', 'twelve_months'.
+            node_handles (List[str]): List of node_handles whose data shall be extracted.
+
+        Returns:
+            Dictionary with all data entries from all given node_handles.
+            Format:
+            Dictionary with node_handles as keys
+                List with all data entries per node_handle
+                    Dictionary with metadata keys and one 'data' key
+                        'data' contains a dictionary with the actual measurements 
+        """
+        if node_handles is None:
+            node_handles = [self.node_handle]
+        if not isinstance(node_handles, list):
+            node_handles = [node_handles]
+        if self.known_data_fields is None:
+            self.get_data_fields()
+        if data_type not in self.known_data_fields:
+            logging.error(f"Data type {data_type} is not known. Current limitation of the API is that only " 
+                           "data of specific type can be retrieved.")
+            return False
+        if date_range not in ['last_hour', 'last_day', 'month', 'twelve_months']:
+            logging.error(f"Date range {date_range} is not known.")
+            return False
+        
+        query = 'sensordata-multiple'
+        payload = {
+            "node_handles": node_handles,
+            "date_range": date_range
+        }
+
+        try:
+            response = self.session.post(f"{url}{query}?data_type={data_type}", json=payload, headers=headers)
+            logging.debug(response.text)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting to retrieve data.")
+            return False
+
+        if not response.ok:
+            logging.error(f"Failed to retrieve data. Status code {response.status_code}")
+            return False
+
+        parsed = json.loads(response.text)
+        return parsed['data']
+        
+    def add_data(self, data, additional_sensors, node_handle=None):
+        """
+        Push a single measurement set to the database.
+        
         Args:
             data (np.array): Collected dataline from the MU
             node_handle (str): Node that collected the dataset, if None, it assumes itself as collector
@@ -196,14 +332,16 @@ class HTTPClient(object):
         
         return True
 
-
-    def add_data2(self, node_handle=None):
+    # FIXME: does not work?
+    def add_data2(self, data, timestamp, node_handle=None):
         """
-        Adds a single measurement set to the database
+        Add a single measurement set to the website.
+        
         Args:
-            node_handle (str): Node that collected the dataset, if None, it assumes itself as collector
+            node_handle (str): Node that collected the data. If None, it assumes itself as collector.
+        
         Returns:
-            True if a successful response is received, False otherwise
+            True if a successful response is received, False otherwise.
         """
         if node_handle is None:
             node_handle = self.node_handle
@@ -212,157 +350,40 @@ class HTTPClient(object):
         payload = {
             "node_handle": node_handle,
             "data": {
-                "temp_external": 0,
-                "light_external": 0,
-                "humidity_external": 0,
-                "differential_potential_ch1": 10,
-                "differential_potential_ch2": 20,
+                "temp_external": 2,
+                "light_external": 2,
+                "humidity_external": 2,
+                "differential_potential_ch1": 11,
+                "differential_potential_ch2": 21,
                 "rf_power_emission": 0,
                 "transpiration": 0,
                 "air_pressure": 0,
                 "soil_moisture": 0,
-                "soil_temperature": 0
+                "soil_temperature": 0,
+                "mag_total": 0
             },
-            "date": "2023-01-23 22:25:00"
+            "date": "2023-11-06 12:32:00"
         }
-        response = None
-        while response is None:
-            try:
-                response = requests.request("POST", url + query, json=payload, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to POST the new data field. Trying again.")
+        
+        try:
+            response = self.session.post(url + query, json=payload, headers=headers)
+        except requests.exceptions.Timeout:
+            logging.error("Timeout while waiting to add the new data")
+            return False
         
         if not response.ok:
-            print(f"ERROR: Adding data. Status code {response.status_code}")
+            logging.error(f"Failed to add data. Status code {response.status_code}")
             return False
         
         return True
-
-
-    def get_data(self, node_handle=None):
-        """
-        Returns all data entries from a specified node
-
-        Args:
-            node_handle ([str]): List of node_handles whose data shall be extracted
-
-        Returns:
-            dictionary with all data entries from all given node_handles
-            Format:
-            Dictionary with node_handles as keys
-                List with all data entries per node_handle
-                    Dictionary with metadata keys and one 'data' key
-                        'data' contains a dictionary with the actual measurements 
-        """
-        if node_handle is None:
-            node_handle = [self.node_handle]
-
-        query = 'sensordata-multiple'
-        payload = {
-            "node_ids": node_handle
-        }
-
-        response = None
-        while response is None:
-            try:
-                response = requests.request("POST", url + query, json=payload, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to retrieve data. Trying again.")
-
-        if not response.ok:
-            print(f"ERROR: Retrieving data. Status code {response.status_code}")
-            return False
-
-        parsed = json.loads(response.text)
-        return parsed['data']
-
-    def get_data_fields(self):
-        """
-        Returns all available data fields with handles
-
-        Args:
-
-        Returns:
-            List of all data fields and their meta data
-        """
-
-        query = 'data-field'
-        response = None
-        while response is None:
-            try:
-                response = requests.request("GET", url + query, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to GET current list of data fields. Trying again.")
-                time.sleep(0.5)
-
-        parsed = json.loads(response.text)
-        return parsed['data']
-
-
-    def delete_data_field(self, data_handle):
-        """
-        Deletes data field with matching handle 
-
-        Args:
-            data_handle (str): handle of data field to delete
-
-        Returns:
-            
-        """
-
-        query = 'data-field/delete'
-        payload = {
-            "handle": data_handle
-        }
-
-        response = None
-        while response is None:
-            try:
-                response = requests.request("POST", url + query, json=payload, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to delete data field. Trying again.")
-                time.sleep(0.5)
-        return True
-
-
-    def delete_node(self, node_handle):
-        """
-        Deletes  node with matching handle 
-
-        Args:
-            node_handle (str): handle of node to delete
-
-        Returns:
-            
-        """
-
-        query = 'nodes/delete'
-        payload = {
-            "handle": node_handle
-        }
-
-        response = None
-        while response is None:
-            try:
-                response = requests.request("POST", url + query, json=payload, headers=headers, timeout=2.0)
-            except requests.exceptions.Timeout:
-                print("Timeout while waiting to delete node. Trying again.")
-                time.sleep(0.5)
-        return True
-
+        
         
 def main():
+    setup_logger('TEST', level=logging.DEBUG)
+    logging.info('Starting HTTP client.')
     
-    client = HTTPClient('test_node_2', 'Test node 2')
-    #client.add_data2("ttyACM0")
-    #print(client.get_data('test_node_2'))
-    #print(client.node_exists("ttyACM0"))
-    #print(client.get_data())
-    #client.delete_data_field("air_pressure")
-    #client.add_data_field("Air Pressure", "air_pressure", "test", node_handle=None)
-    client.delete_node("ttyACM0")
-    client.delete_node("test_node_2")
-    #print(client.get_data(['test_node_2']))
+    client = HTTPClient('dev', 'Development Node')
+
 
 if __name__ == '__main__':
     main()
